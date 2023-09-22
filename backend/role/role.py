30cc4f74c7b7a9192f0ea3fbfac539d730b03f67
Shannevie Teo <llteo.2021@scis.smu.edu.sg>
2023-09-23 00:56:12 +0800

    SBRP-46 Updated role.py to include employment_type

--- backend/role/role.py
+++ backend/role/role.py
@@ -17,13 +17,14 @@ CORS(app)
 
 
 class Role(db.Model):
-    tablename = 'roles'
+    __tablename__ = 'roles'
 
     role_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False, unique=True)
     experience = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(50), nullable=False)
     department = db.Column(db.String(50), nullable=False)
+    employment_type = db.Column(db.String(50), nullable=False)
     requirement = db.Column(db.String(256), nullable=False)
     description = db.Column(db.String(256), nullable=False)
     hiring_manager = db.Column(db.String(256), nullable=False)
@@ -32,14 +33,14 @@ class Role(db.Model):
     modified = db.Column(db.DateTime, nullable=False,
                          default=datetime.now, onupdate=datetime.now)
 
-    role_skills = db.relationship("Role_Skill", primaryjoin='Role.name == foreign(Role_Skill.role_name)',
-                                  foreign_keys="[Role_Skill.role_name]", backref="role")
+    role_skills = db.relationship("Role_Skill", primaryjoin='Role.name == foreign(Role_Skill.role_name)', foreign_keys="[Role_Skill.role_name]", backref="role")
 
-    def __init__(self, name, experience, location, department, requirement, description, hiring_manager, deadline, skills):
+    def __init__(self, name, experience, location, department, employment_type, requirement, description, hiring_manager, deadline, skills):
         self.name = name
         self.experience = experience
         self.location = location
         self.department = department
+        self.employment_type = employment_type
         self.requirement = requirement
         self.description = description
         self.hiring_manager = hiring_manager
@@ -56,6 +57,7 @@ class Role(db.Model):
             'experience': self.experience,
             'location': self.location,
             'department': self.department,
+            'employment_type': self.employment_type,
             'requirement': self.requirement,
             'description': self.description,
             'hiring_manager': self.hiring_manager,
@@ -72,7 +74,7 @@ class Role(db.Model):
 
 
 class Role_Skill(db.Model):
-    tablename = 'role_skills'
+    __tablename__ = 'role_skills'
 
     role_name = db.Column(db.String(20), primary_key=True)
     skill_name = db.Column(db.String(20), primary_key=True)
@@ -91,14 +93,12 @@ with app.app_context():
     existing_role_1 = db.session.query(Role).filter(
         Role.name == "Software Engineer").first()
     if not existing_role_1:
-        new_role_1 = Role(name="Software Engineer", experience="Entry Level", location="Singapore", department="Information Technology",
-                          requirement="Bachelors Degree in Computer Science", description="Description", hiring_manager="Tony Stark", deadline="2023-12-31", skills=["HTML", "CSS", "JavaScript"])
-        new_role_2 = Role(name="Marketing Associate", experience="Entry Level", location="Singapore", department="Marketing",
-                          requirement="Bachelors Degree", description="Description", hiring_manager="Tony Stark", deadline="2023-12-31", skills=["Graphic Design"])
-        new_role_3 = Role(name="Sales Associate", experience="Entry Level", location="Singapore", department="Sales",
-                          requirement="Bachelors Degree", description="Description", hiring_manager="Tony Stark", deadline="2023-12-31", skills=[])
-        
-
+        new_role_1 = Role(name="Software Engineer", experience="Entry Level", location="Singapore", department="Information Technology", employment_type="Full Time",
+                          requirement="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo", description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida quis blandit turpis cursus in hac.", hiring_manager="Tony Stark", deadline="2023-12-31", skills=["HTML", "CSS", "JavaScript"])
+        new_role_2 = Role(name="Marketing Associate", experience="Entry Level", location="Singapore", department="Marketing", employment_type="Full Time",
+                          requirement="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo", description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida quis blandit turpis cursus in hac.", hiring_manager="Tony Stark", deadline="2023-12-31", skills=["Graphic Design"])
+        new_role_3 = Role(name="Sales Associate", experience="Entry Level", location="Singapore", department="Sales", employment_type="Full Time",
+                          requirement="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo", description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida quis blandit turpis cursus in hac.", hiring_manager="Tony Stark", deadline="2023-12-31", skills=[])
 
         db.session.add_all([new_role_1, new_role_2, new_role_3])
         db.session.commit()
@@ -141,6 +141,7 @@ def find_by_role_id(role_id):
         }
     ), 404
 
+
 @app.route("/role", methods=['POST'])
 def create_role():
     data = request.get_json()
@@ -175,7 +176,5 @@ def create_role():
         }
     ), 201
 
-if name == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
-
-
