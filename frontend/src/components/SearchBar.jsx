b458cc419eb30b9300c46563475e6f90b263f300
Shannevie Teo <llteo.2021@scis.smu.edu.sg>
2023-10-05 11:26:38 +0800

    SBRP-71 Added the search functionality

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,61 @@
+// General imports
+import { useState } from "react";
+
+// Chakra imports
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
+
+// Icons
+import { BiSearch, BiX } from "react-icons/bi";
+
+export default function SearchBar({ onSearchChange }) {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+    onSearchChange(newValue);
+  };
+
+  const resetSearch = () => {
+    setValue("");
+    onSearchChange("");
+  };
+
+  return (
+    <InputGroup>
+      <InputLeftElement pointerEvents="none" color="gray.500">
+        <BiSearch />
+      </InputLeftElement>
+      <Input
+        value={value}
+        onChange={handleChange}
+        variant={"filled"}
+        size={"md"}
+        fontSize={"sm"}
+        placeholder={"Search..."}
+        backgroundColor={"gray.200"}
+        _dark={{
+          backgroundColor: "gray.700",
+          _placeholder: { color: "gray.400" },
+        }}
+        focusBorderColor={"pink.300"}
+        _placeholder={{ opacity: 0.7, color: "gray.500" }}
+      />
+      {value && (
+        <InputRightElement
+          color="gray.500"
+          fontSize={"lg"}
+          onClick={resetSearch}
+          cursor={"pointer"}
+        >
+          <BiX />
+        </InputRightElement>
+      )}
+    </InputGroup>
+  );
+}
