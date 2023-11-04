68849fbe1afae1b9b9e6b5261491918b1435af2b
ngkangting <45414933+ngkangting@users.noreply.github.com>
2023-11-04 21:37:40 +0800

    SBRP-9 Update unit_tests.py

--- backend/skill/unit_tests.py
+++ backend/skill/unit_tests.py
@@ -4,13 +4,6 @@ from skill import Skill
 
 
 class TestSkill(unittest.TestCase):
-    def test_get_skill_name(self):
-        s1 = Skill(
-            skill_name="Account Management",
-            description="Description",
-        )
-        self.assertEqual(s1.get_skill_name(), "Account Management")
-
     def test_to_json(self):
         s1 = Skill(
             skill_name="Account Management",
